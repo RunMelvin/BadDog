3e52b13a27edeccd2d5cebab4a7c4d7345a51451
Chris Melvin <runfree.melvin@gmail.com>
2013-01-31 16:18:01 -0500

    Switch content type to html

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end("My name is Ralph and I am a bad dog. Sometimes I just can't help myself. I try to be good but I can't. My doggy instincts get the best of me and I can't control myself. I must steal food from kids. I must chew things I shouldn't. I must chase the cat, the squirrel, the rabbit, the bird. It's who I am.")
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end("<html><body><p>My name is Ralph and I am a bad dog. Sometimes I just can't help myself. I try to be good but I can't. My doggy instincts get the best of me and I can't control myself. I must steal food from kids. I must chew things I shouldn't. I must chase the cat, the squirrel, the rabbit, the bird. It's who I am.</p></body></html>")
 }).listen(port);
