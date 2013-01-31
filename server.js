085cd487ae509247af2e2fc45e8d8afa564c7f9d
Chris Melvin <runfree.melvin@gmail.com>
2013-01-31 16:26:43 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write("<html><body><p>My name is Ralph and I am a bad dog. Sometimes I just can't help myself. I try to be good but I can't. My doggy instincts get the best of me and I can't control myself. I must steal food from kids. I must chew things I shouldn't. I must chase the cat, the squirrel, the rabbit, the bird. It's who I am.</p></body></html>");
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write("My name is Ralph and I am a bad dog. Sometimes I just can't help myself. I try to be good but I can't. My doggy instincts get the best of me and I can't control myself. I must steal food from kids. I must chew things I shouldn't. I must chase the cat, the squirrel, the rabbit, the bird. It's who I am.");
   res.end()
 }).listen(port);
